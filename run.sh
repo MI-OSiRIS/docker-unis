0b854ca259da8219cb6f69fc47be695e29d2dd4d
Ezra Kissel <ezkissel@indiana.edu>
2019-11-08 12:05:57 -0500

    allow alternative UNIS ports to be given as env vars

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 HOSTNAME=`hostname`
+UNIS_PORT=8888
+
+[ ! -z "$PORT" ] && UNIS_PORT=$PORT
 
 sudo /etc/init.d/mongodb start
 sudo /etc/init.d/redis-server start
 
 echo "UNIS IP : `hostname --ip-address`"
-periscoped --port 30100 -d DEBUG -c /etc/periscope/unis.cfg
+periscoped --port ${UNIS_PORT} -d DEBUG -c /etc/periscope/unis.cfg
